2376ad7f956e35ec1aa5cc10895b50cb0882c537
egor.chasnovskii <egor.chasnovskii@gmail.com>
2019-12-04 09:27:30 +0200

    add items to full-size completed work slider

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,6 @@
 <?php
 $errors = '';
-$myemail = 'egor.chasnovskii@gmail.com';//<-----Put Your email address here.
+$myemail = 'remont.pod.kliuch.kyiv@gmail.com';
 if(empty($_POST['name'])  ||
    empty($_POST['phone']))
 {
@@ -13,11 +13,11 @@ if( empty($errors))
 {
 $to = $myemail;
 $email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Phone \n $phone";
+$email_body = "На сайте новое сообщение: ".
+" Детали:\n Имя: $name \n ".
+"Телефон \n $phone";
 $headers = "From: $myemail\n";
-$email_address = "egorindynamo@gmail.com";
+$email_address = "egor.chasnovskii@gmail.com";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
